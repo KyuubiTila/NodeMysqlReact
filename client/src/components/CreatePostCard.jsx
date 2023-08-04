2d166e26c6054db6e25aa8426777bff2f88546fc
Akingbade Ayotunde <tylerkatsumoto@gmail.com>
2023-08-04 21:55:53 +0100

    Form validation using Formik and Yup

--- client/src/components/CreatePostCard.jsx
+++ client/src/components/CreatePostCard.jsx
@@ -1,73 +1,84 @@
 import React from 'react';
-
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 const CreatePostCard = ({
-  setTitle,
-  setPostText,
-  setUsername,
   addProductHandler,
+  initialValues,
+  validationSchema,
 }) => {
   return (
-    <form
+    <Formik
+      initialValues={initialValues}
       onSubmit={addProductHandler}
-      method="POST"
-      encType="multipart/form-data"
+      validationSchema={validationSchema}
     >
-      <div className="mb-6">
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          TITLE
-        </label>
-        <input
-          type="text"
-          id="title"
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="enter title"
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          required
-        />
-      </div>
-      <div className="mb-6">
-        <label
-          htmlFor="password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          POST TEXT
-        </label>
-        <input
-          type="text"
-          id="postText"
-          onChange={(e) => setPostText(e.target.value)}
-          placeholder="enter post"
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          required
-        />
-      </div>
-      <div className="mb-6">
-        <label
-          htmlFor="repeat-password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          USERNAME
-        </label>
-        <input
-          type="text"
-          id="username"
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="enter username"
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          required
-        />
-      </div>
+      <Form>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            TITLE
+          </label>
+          <Field
+            autoComplete="off"
+            type="text"
+            name="title"
+            placeholder="enter title"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required
+          />
+          <span className="text-red-500">
+            <ErrorMessage name="postText" component="span" />
+          </span>
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            POST TEXT
+          </label>
+          <Field
+            autoComplete="off"
+            type="text"
+            name="postText"
+            placeholder="enter post"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required
+          />
+          <span className="text-red-500">
+            <ErrorMessage name="postText" component="span" />
+          </span>
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="repeat-password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            USERNAME
+          </label>
 
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Add POST
-      </button>
-    </form>
+          <Field
+            autoComplete="off"
+            type="text"
+            name="username"
+            placeholder="enter username"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required
+          />
+          <span className="text-red-500">
+            <ErrorMessage name="postText" component="span" />
+          </span>
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Add POST
+        </button>
+        {/* </form> */}
+      </Form>
+    </Formik>
   );
 };
 
