5e272ebb756226359cab7b35bb781190c527284d
Akingbade Ayotunde <tylerkatsumoto@gmail.com>
2023-08-05 15:29:57 +0100

    redering comments associated with a post on a post

---
+++ client/src/components/CreateCommentCard.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+const CreateCommentCard = ({ initialValues, validationSchema }) => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={''}
+    >
+      <Form>
+        <hr />
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            COMMENT
+          </label>
+          <Field
+            autoComplete="off"
+            type="text"
+            name="comment"
+            placeholder="enter title"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required
+          />
+          <span className="text-red-500">
+            <ErrorMessage name="comment" component="span" />
+          </span>
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="repeat-password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            USERNAME
+          </label>
+
+          <Field
+            autoComplete="off"
+            type="text"
+            name="username"
+            placeholder="enter username"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            required
+          />
+          <span className="text-red-500">
+            <ErrorMessage name="username" component="span" />
+          </span>
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Add Comment
+        </button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default CreateCommentCard;
--- client/src/pages/IndividualPost.jsx
+++ client/src/pages/IndividualPost.jsx
@@ -2,27 +2,40 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import CreateCommentCard from '../components/CreateCommentCard';
+import * as Yup from 'yup';
+import CommentCard from '../components/CommentCard';
 
 const IndividualPost = () => {
   const navigate = useNavigate();
   const { id } = useParams();
   const [title, setTitle] = useState('');
-  const [postText, setPostText] = useState(0);
-  const [username, setUsername] = useState(0);
+  const [postText, setPostText] = useState('');
+  const [username, setUsername] = useState('');
+  const [comments, setComments] = useState([]);
+
+  const initialValues = {
+    comment: '',
+    username: '',
+  };
+  const validationSchema = Yup.object().shape({
+    comment: Yup.string().required('you must send a comment'),
+    username: Yup.string().required('you must add your username'),
+  });
 
   // USE EFFECT CONNECTING TO DATABASE GETTING THE DATA WHICH WOULD THE BE HELD BY THE USEEFFCT AND THROWN TO THE FRONT END
   useEffect(() => {
     // ASYNC FUNCTION TO CALLING THE DATABASE
     const getIndividualPost = async () => {
       try {
-        const response = await axios.get(
+        const postResponse = await axios.get(
           `http://localhost:3001/api/posts/${id}`
         );
+        console.log(postResponse);
         // Destructure 'data' from the 'response' object
-        const { data } = response;
-        console.log(response);
+        const { data } = postResponse;
+        console.log(postResponse);
         //SETTING THE RESPONSE DATAS TO THE USESTATE FOR HOLDING
-        console.log(data);
         setTitle(data.title);
         setPostText(data.postText);
         setUsername(data.username);
@@ -33,48 +46,81 @@ const IndividualPost = () => {
     getIndividualPost();
   }, [id]);
 
+  useEffect(() => {
+    // ASYNC FUNCTION TO CALLING THE DATABASE
+    const gePostsComments = async () => {
+      try {
+        const commentResponse = await axios.get(
+          `http://localhost:3001/api/posts/${id}/comment`
+        );
+        const { data } = commentResponse;
+        setComments(data);
+        console.log(commentResponse);
+        console.log(comments);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    gePostsComments();
+  }, [id]);
+
   const handleDelete = async (id) => {
     await axios.delete(`http://localhost:3001/api/posts/${id}`);
     navigate('/posts');
   };
 
-  //   const handleReviewDelete = async (reviewId) => {
-  //     await axios.delete(
-  //       `http://localhost:8080/api/reviews/deleteReview/${reviewId}`
-  //     );
-  //   };
+  const handleCommentDelete = async (id) => {
+    await axios.delete(`http://localhost:3001/api/comments/${id}`);
+    navigate('/individualpost/:id');
+  };
 
   return (
-    <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-      {/* <Link to={`/productDetail/${id}`}>
+    <div className="flex flex-wrap justify-center lg:flex-row">
+      <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        {/* <Link to={`/productDetail/${id}`}>
         <img src={'http://localhost:8080/' + image} alt="" />
       </Link> */}
-      <div className="p-5">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Post Title : {title}
-        </h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Post Text : {`${postText}`}
-        </p>
+        <div className="p-5">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Post Title : {title}
+          </h5>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Post Text : {`${postText}`}
+          </p>
 
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Creator Username : {username}
-        </p>
-        {/* <Link to={`/editProduct/${id}`} className="relative">
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Creator Username : {username}
+          </p>
+          {/* <Link to={`/editProduct/${id}`} className="relative">
           <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
               Edit
             </span>
           </button>
         </Link> */}
-        <button
-          onClick={() => handleDelete(id)}
-          className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
-        >
-          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            Delete
-          </span>
-        </button>
+          <button
+            onClick={() => handleDelete(id)}
+            className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+          >
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Delete
+            </span>
+          </button>
+        </div>
+        {comments.map((eleement) => {
+          return (
+            <CommentCard
+              key={eleement.id}
+              eleement={eleement}
+              handleCommentDelete={handleCommentDelete}
+            />
+          );
+        })}
+
+        <CreateCommentCard
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+        />
       </div>
     </div>
   );
