1bdcc0c7318e74ce151bd38f4f21dc91f1aa4def
Akingbade Ayotunde <tylerkatsumoto@gmail.com>
2023-08-06 01:34:33 +0100

    UserModel for registering users

---
+++ server/models/UsersModel.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  // "Users" as the name it should place the table as
+  const Users = sequelize.define('Users', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  // POSTS TABLE ASSOCIATION WITH COMMENTS TABLE, hence creating the PostId on the comment table in respect to the post ID
+  Users.associate = (models) => {
+    Users.hasMany(models.Posts, {
+      onDelete: 'cascade',
+    });
+  };
+  return Users;
+};
